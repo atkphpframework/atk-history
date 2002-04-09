90bbbc52aee67b6ddbdbbf72ed2e1ba92d1b7929
(no author) (SVN Import) <info@achievo.org>
2002-04-09 14:05:36 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-4.8.6'.

--- javascript/newwindow.js
+++
@@ -1,8 +0,0 @@
- 
-function NewWindow(mypage, myname, w, h, scroll, resize) {
-  var winl =  10;
-  var wint =  10;
-  winprops = "height="+h+",width="+w+",top="+wint+",left="+winl+",scrollbars="+scroll+","+resize
-  win = window.open(mypage, myname, winprops)
-  if (parseInt(navigator.appVersion) >= 4) { win.window.focus(); }
-}
