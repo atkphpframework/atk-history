e3eff5bd6e8373c8822edb6c2c600cea17f8f6c3
boy (SVN Import) <info@achievo.org>
2007-08-23 13:39:23 +0000

    Updated Galactus branch with HEAD Branch merge HEAD => galactus

--- javascript/class.atkdialog.js
+++ javascript/class.atkdialog.js
@@ -11,27 +11,35 @@ ATK.Dialog.prototype = {
   initialize: function(title, url, theme, options) {
     this.title = title;
     this.url = url;
-    this.theme = theme || 'alphacube';    
-    this.options = this.options || {};
+    this.theme = theme || 'alphacube';
+    this.options  = options || {};
  },
-  
+
   /**
    * Eval JavaScript in response.
    */
   evalResponse: function(transport) {
-    transport.responseText.evalScripts();
+    setTimeout(function() { transport.responseText.evalScripts() }, 10);
   },
-  
+
   /**
    * Used internally.
    */
   onShow: function(transport) {
     this.evalResponse(transport);
-    
-    if (this.options.width || this.options.height)
-      setTimeout(this.resize.bind(this), 500);
+    if (this.options.width || this.options.height) {
+      this.delayedResize();
+    }
   },
-  
+
+  /**
+   * Delayed auto-resize. Sometimes needed because the content is not always
+   * fully updated yet (you don't always know how long it takes to update the DOM).
+   */
+  delayedResize: function() {
+    setTimeout(this.resize.bind(this), 100);
+  },
+
   /**
    * Auto-resize the dialog.
    */
@@ -40,30 +48,54 @@ ATK.Dialog.prototype = {
 
     var d = Element.getDimensions(element);
     var p = Position.cumulativeOffset(element);
-    
-    var window = Windows.getWindow(Dialog.dialogId);    
+
+    var window = Windows.getWindow(Dialog.dialogId);
     window.setSize(d.width, d.height);
-    window.setLocation(p[1] - window.heightN, p[0] - window.widthW);       
+    window.setLocation(p[1] - window.heightN, p[0] - window.widthW);
   },
-  
+
+  /**
+   * Serialize form.
+   */
+  serializeForm: function() {
+    var elements = Form.getElements(this.options.serializeForm);
+    var queryComponents = new Array();
+
+    for (var i = 0; i < elements.length; i++) {
+      if (elements[i].name && elements[i].name.substring(0, 3) != 'atk') {
+        var queryComponent = Form.Element.serialize(elements[i]);
+        if (queryComponent)
+          queryComponents.push(queryComponent);
+      }
+    }
+
+    return queryComponents.join('&');
+  },
+
   /**
    * Show dialog.
    */
   show: function() {
     ATK.Dialog.stack.push(this);
-    
+
     var windowParameters = { className: this.theme, title: this.title };
     if (this.options.width)
       windowParameters['width'] = this.options.width;
     if (this.options.height)
       windowParameters['height'] = this.options.height;
-    
+
+    var options = {};
+    options['onSuccess'] = this.onShow.bind(this);
+    if (this.options.serializeForm) {
+      options['parameters'] = this.serializeForm();
+    }
+
     Dialog.info(
-      { url: this.url, options: { onSuccess: this.onShow.bind(this) } },
+      { url: this.url, options: options },
       { windowParameters: windowParameters }
-    );    
+    );
   },
-  
+
   /**
    * Save dialog contents using an Ajax request to the given URL.
    * The given extraParams (optional) will be appended at the end of the
@@ -71,22 +103,22 @@ ATK.Dialog.prototype = {
    */
   save: function(url, form, extraParams, options) {
     var params = Form.serialize(form);
-    
+
     if (extraParams != null)
       params += '&' + $H(extraParams).toQueryString();
-      
+
     if (window.getCurrentTab)
     	params += '&atkparenttab=' + getCurrentTab();
 
-    var options = options || {};  
-      
+    var options = options || {};
+
     var evalFunc = this.evalResponse.bind(this);
     var successFunc = options['onSuccess'] || function() { };
-        
+
     var options = options || {};
     options['parameters'] = params;
     options['onSuccess'] = function(transport) { evalFunc(transport); successFunc(transport); };
-    
+
     new Ajax.Request(url, options);
   },
 
@@ -96,16 +128,16 @@ ATK.Dialog.prototype = {
   saveAndClose: function(url, form, extraParams) {
     this.save(url, form, extraParams, { onSuccess: this.close.bind(this) });
     this.close();
-  },  
-  
+  },
+
   /**
    * Close / cancel dialog.
    */
   close: function() {
-    Dialog.closeInfo();
     ATK.Dialog.stack.pop();
+    Dialog.closeInfo();
   },
-  
+
   /**
    * Update dialog contents.
    */
